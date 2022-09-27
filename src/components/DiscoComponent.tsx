015b90d7d92090fd5b4286558c4578231295ecc5
Eric Chen <eric@disco.xyz>
2022-09-27 16:35:17 -0400

    Updates for adding accounts and credentials

--- src/components/DiscoComponent.tsx
+++ src/components/DiscoComponent.tsx
@@ -9,11 +9,17 @@ type Props = {
 
 export default function DiscoComponent({isOpen}: Props) {
   // const [pageState, setPageState] = useState('');
+  const DISCONAUT_CREDENTIAL = "OfficialDisconautCredential";
   const [profileFound, setProfileFound] = useState('fetching');
   const { account } = useEthers();
   // console.log("loaded dot env:" + process.env.REACT_APP_BEARER_TOKEN);
   const [name, setName] = useState('');
   const [bio, setBio] = useState('');
+  const [twitter, setTwitter] = useState('');
+  const [discord, setDiscord] = useState('');
+  const [domain, setDomain] = useState('');
+  const [numCreds, setNumCreds] = useState(0);
+  const [disconautCred, setDisconautCred] = useState(false);
 
   const getDiscoProfile = async () => {
     // const base64 = require('base-64');
@@ -52,8 +58,25 @@ export default function DiscoComponent({isOpen}: Props) {
         console.log(user_info);
         setName(user_info["profile"].name);
         setBio(user_info["profile"].bio);
+        setTwitter(user_info["linkages"]["twitter"].id);
+        setDiscord(user_info["linkages"]["discord"].id);
+        setDomain(user_info["linkages"]["dns"].id);
         // setPageState('fetched');
         setProfileFound('found!');
+
+        //check for credentials.
+        if (user_info["creds"].length > 0) {
+          setNumCreds(user_info["creds"].length);
+          //iterate thru array if credential is detected, set disconaut cred to true
+        
+          user_info["creds"].forEach((cred: { [x: string]: string | string[]; }) => {
+            console.log(cred['type']);
+            if (cred['type'].includes(DISCONAUT_CREDENTIAL)) {
+              console.log("credential found");
+              setDisconautCred(true);
+            }
+          });
+        }
       } else { 
         console.log("Profile not found!")
         setProfileFound('not found');
@@ -72,6 +95,7 @@ export default function DiscoComponent({isOpen}: Props) {
               {account}
         </div>
         <h3>Searching for Disco User Profile...</h3>
+        <h4> Checking for credentials...</h4>
         {/* <div> {getDiscoProfile()} </div>  */}
         <Button
           variant='outline-secondary'
@@ -95,7 +119,7 @@ export default function DiscoComponent({isOpen}: Props) {
             </div>
           </div>
           <div>
-            Sorry, no valid Disco Profile found for your ETH address. Please sign up 
+            Sorry, no valid Disco Profile found for your ETH address. Please create one
             <Link href="http://app.disco.xyz" target="_blank"> here.</Link>
           </div>
         </div>
@@ -122,12 +146,36 @@ export default function DiscoComponent({isOpen}: Props) {
           </div>
           <div className='form-row'>
             <div>
-            <label>Name: </label>
-              {name}
+              <label>Name: </label>
+                {name}
             </div>
             <div>
-            <label>Bio: </label>
-              {bio}
+              <label>Bio: </label>
+                {bio}
+            </div>
+          </div>
+          <br/>
+          <h2> <b> Your connected accounts: </b></h2>
+          <div>
+            <div>
+              <label> Twitter: </label>
+                {twitter}
+            </div>
+            <div>
+              <label> Discord: </label>
+                {discord}
+            </div>
+            <div>
+              <label> Domain: </label>
+                {domain}
+            </div>
+          </div>
+          <br/>
+          <h2> <b> {numCreds} Credentials Found! </b></h2>
+          <div>
+            <div>
+              <label> Disco Credential: </label>
+                {disconautCred}
             </div>
           </div>
         </div>
